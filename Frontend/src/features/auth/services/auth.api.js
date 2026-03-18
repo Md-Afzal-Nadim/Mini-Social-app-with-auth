db224eafc638b1cda0a10389ce4aa83a6707726e
afzal <write1afzal@gmail.com>
2026-03-19 04:42:07 +0530

    update file

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 
 const api = axios.create({
-  baseURL: 'http://localhost:3000/api/auth',
+  baseURL: 'https://mini-social-app-with-auth.onrender.com/api/auth',
   withCredentials:true,
 })
 
