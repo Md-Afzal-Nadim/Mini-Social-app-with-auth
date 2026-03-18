6bf9794bbbe95827756cf4b34a15ee7d0720cf20
afzal <write1afzal@gmail.com>
2026-03-19 04:59:17 +0530

    origin update

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
   credentials: true,
-  origin: "http://localhost:5173"
+  origin: ["http://localhost:5173","https://mini-social-app-with-auth.onrender.com"]
 }));
 
 /* require router */
